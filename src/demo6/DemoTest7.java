9b2e3bd2f9a7846be1428cc6536c35b47868c519
Esesm <Esesm@LAPTOP-JL4SL9QM.home>
2023-07-28 23:16:06 -0400

    Initial Commit

---
+++ src/demo6/DemoTest7.java
@@ -0,0 +1,15 @@
+package demo6;
+
+public class DemoTest7 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the new art class");
+		
+		String backpack [] = {"Pencils", "brushes", "Paint"};
+		String typeOfArt [] = {"Painting", "Sculpture", "Expressionism"};
+		
+		System.out.println("Type of material: " + backpack [2]);
+		System.out.println("Type of art: " + typeOfArt [0]);
+		
+	}
+}
